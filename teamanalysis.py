d68db5cac3e16b9116a3c5e1833dd8f9e6e3ae59
SamuelHabtu <SGmagicsam@gmail.com>
2023-11-14 04:28:00 -0700

    Updated algorithms testing params

--- teamanalysis.py
+++ teamanalysis.py
@@ -1,6 +1,8 @@
 import csv
 import itertools
 import random
+import multiprocessing
+n_starts = 0
 
 def randomStart(players, teamsize = 15):
     return random.sample(players,teamsize)
@@ -13,35 +15,117 @@ def evaluateSquad(cur_squad, potential_squad):
         win_counter += battle(cur_avgs, avgs, category)
     return win_counter
 
-def hillClimb(players,num_restarts = 5000, max_iterations = 100, team_size = 15):
 
-    best_squad = None
-    best_score = float("-inf")
+def geneticOptimization(players, population_size = 2400, generations = 25000, mutation_rate = 0.1, crossover_rate = 0.07):
+
+    population = initializePopulation(players)
+    best_fitness = float("-inf")
+    for generation in range(generations):
+        print(f"Generation #{generation + 1}")
+        fitness_scores = [sum(normalizedScore(individual)) for individual in population]
+        selected_parents = [tournamentSelection(population) for _ in range(population_size)]
+        sorted_population = [x for _, x in sorted(zip(fitness_scores, population), key=lambda pair: pair[0], reverse=True)]
+        top_squads = sorted_population[:population_size]
+
+        new_population = []
+        for i in range(0, population_size,2):
+            parent_one = selected_parents[i]
+            parent_two = selected_parents[i + 1]
+            if random.uniform(0, 1) < crossover_rate:
+                child_one  = crossOver(parent_one, parent_two)
+                child_two = crossOver(parent_one, parent_two)
+                new_population = new_population + [mutate(players, child_one), mutate(players, child_two)]
+            else:
+                new_population = new_population + [mutate(players,parent_one), mutate(players, parent_two)]
+        population = new_population[:]
+        current_best_fitness = max(fitness_scores)
+        if current_best_fitness > best_fitness:
+            best_fitness = current_best_fitness
+            best_individual = sorted_population[0]
+    return best_individual
+
+def initializePopulation(players, population_size = 100, team_size = 15):
+    #create teams equal to the number of populations
+    populations = []
+    for i in range(population_size):
+        populations.append(randomStart(players))
+    return populations
+
+def tournamentSelection(population, tournament_size = 3):
+    participants = random.sample(population, tournament_size)
+    return max(participants, key = normalizedScore)
+
+def crossOver(parent_1, parent_2):
+    crossover_point = random.randint(1, len(parent_1) - 1)
+    
+    # The crossover operation
+    child = parent_1[:crossover_point] + [player for player in parent_2 if player not in parent_1[:crossover_point]]
+    
+    # Adjust the child size if it's too large
+    if len(child) > len(parent_1):
+        child = child[:len(parent_1)]
+
+    return child
+def mutate(players, individual):
+    mutated_player = random.choice(players)
+    #avoiding repeated players(we wouldnt want the GOATED squad of all mitchell robinsons afterall)
+    while mutated_player in individual:
+        mutated_player = random.choice(players)
+    index_to_mutate = random.randint(0, len(individual) - 1)
+
+    individual[index_to_mutate] = mutated_player
+    return individual
+
+
+def hillClimb(players, num_restarts=1200, max_iterations=8000, team_size=15, num_processes=4):
+    print(f"searching through: {len(players)} players, with {num_restarts} restarts and {max_iterations} iterations split up into {num_processes} threads")
+    pool = multiprocessing.Pool(processes=num_processes)
+    results = pool.starmap(hillClimbSingleRun, [(players, team_size, max_iterations) for _ in range(num_restarts)])
+    pool.close()
+    pool.join()
+    best_squad = max(results, key=lambda squad: sum(normalizedScore(squad)))    
+    return best_squad
+
+def hillClimbSingleRun(players, team_size, max_iterations):
+    cur_squad = randomStart(players, team_size)
+    cur_score = sum(normalizedScore(cur_squad))
+    for _ in range(max_iterations):
+        neighbour_squad = cur_squad[:]
+        index_to_swap = random.randint(0, team_size - 1)
+        new_player = random.choice([p for p in players if p not in cur_squad])
+        neighbour_squad[index_to_swap] = new_player
+        neighbor_score = sum(normalizedScore(neighbour_squad))
+        if neighbor_score > cur_score:
+            cur_squad = neighbour_squad[:]
+            cur_score = neighbor_score
+    return cur_squad
+
+def altHillClimb(players,num_restarts = 10000, max_iterations = 5000, team_size = 15):
+    
+    best_squad = optimize(extractPlayers("Jimmy's Buckets.csv"))
     for _ in range(num_restarts): 
-        #print(f"Start #{_ + 1}")
+        print(f"Start #{_ + 1}") 
         cur_squad = randomStart(players, team_size)
-        cur_score = sum(normalizedScore(cur_squad))
         for _ in range(max_iterations):
             neighbour_squad = cur_squad.copy()
             index_to_swap = random.randint(0, team_size - 1)
             new_player = new_player = random.choice([p for p in players if p not in cur_squad])  # Exclude players already in squad
             neighbour_squad[index_to_swap] = new_player
-            neighbour_score = sum(normalizedScore(neighbour_squad))
-            if neighbour_score > cur_score:
+            cur_score = evaluateSquad(cur_squad, neighbour_squad)
+            if evaluateSquad(cur_squad, neighbour_squad) > evaluateSquad(neighbour_squad, cur_squad):
                 cur_squad = neighbour_squad[:]
-                cur_score = neighbour_score
-                if cur_score > best_score:
+                if evaluateSquad(best_squad, cur_squad) > evaluateSquad(cur_squad, best_squad):
                     best_squad = cur_squad[:]
-                    best_score = cur_score
-
-
-    
+                
     return best_squad
 
+    
 
 def averages(team):
 
     averages = {}
+    if not team:
+        print(f"None got sent into averages? LOL")
     total_fga = 0
     total_3pa = 0
     total_fgm = 0
@@ -66,6 +150,8 @@ def averages(team):
         total_to += player["TO"]
         total_pf += player["PF"]
 
+    if not total_3pa:
+        total_3pa = float("0.000000000000000002")
     averages["FG%"] = total_fgm/total_fga
     averages["3PT%"] = total_3pm/total_3pa
     averages["REB"] = total_reb#/len(team)
@@ -98,12 +184,15 @@ def optimize(players, teamsize = 15):
     '''
     return current_squad
 
-def optimize_greedy(players, teamsize=15):
-    current_squad = players[:teamsize]
-    for i in range(len(teamsize)):
-        for j in range(teamsize,len(players)):
-            temp_squad = current_squad[:]
-            temp_squad[i], temp_squad[j] = temp_squad[i], temp_squad[j]
+def bruteForce(players, teamsize=15):
+    best_squad = None
+    best_score = float("-inf")
+    for squad in (itertools.combinations(players, 15)):
+        score = sum(normalizedScore(squad))
+        if score > best_score:
+            best_squad = squad[:]
+            best_score = score
+    return best_squad
 
 def headToHead(current_squad, temp_squad):
     #returns ture if a team is better than the other
@@ -125,56 +214,56 @@ def normalizedScore(squad):
 
     stats = averages(squad)
     min_FG_percent = 0
-    max_FG_percent = 0.55
+    max_FG_percent =  0.5018210197710717
     
     min_ThreePt_percent = 0
-    max_ThreePt_percent = 0.39
+    max_ThreePt_percent = 0.3761388286334057
     
     min_REB = 0
-    max_REB = 6500  # Assuming this is the upper limit for rebounds 
+    max_REB = 386.5 # Assuming this is the upper limit for rebounds 
     min_AST = 0
-    max_AST = 3800  # You'll need to determine the maximum possible value for AST based on your league settings
+    max_AST = 232.60000000000002  # You'll need to determine the maximum possible value for AST based on your league settings
     min_STL = 0
-    max_STL = 1400.0  # You'll need to determine the maximum possible value for STL based on your league settings
+    max_STL = 58.7  # You'll need to determine the maximum possible value for STL based on your league settings
     min_BLK = 0
-    max_BLK = 831.5999999999999  # You'll need to determine the maximum possible value for BLK based on your league settings
+    max_BLK =  39.7 # You'll need to determine the maximum possible value for BLK based on your league settings
     min_AT = 0
-    max_AT = 2.41  # You'll need to determine the maximum possible value for A/T based on your league settings
+    max_AT = 2.09027780351422 # You'll need to determine the maximum possible value for A/T based on your league settings
     min_PF = 0  # You'll need to determine the minimum possible value for PF based on your league settings
-    max_PF = -1900
+    max_PF = -136.10000000000002
     # Normalize each statistic, each stat is also weighted by 0.125
     normalized_stats = []
     normalized_stats.append((stats["FG%"] - min_FG_percent) / (max_FG_percent - min_FG_percent)*0.125)
     normalized_stats.append((stats["3PT%"] - min_ThreePt_percent) / (max_ThreePt_percent - min_ThreePt_percent)*0.125)
     normalized_stats.append((stats["REB"] - min_REB) / (max_REB - min_REB)*0.125)
     normalized_stats.append((stats["AST"] - min_AST) / (max_AST - min_AST)*0.125)
-    normalized_stats.append((stats["STL"] - min_STL) / (max_STL - min_STL)*0.125)
+    normalized_stats.append((stats["STL"] - min_STL) / (max_STL - min_STL)*0.125) 
     normalized_stats.append((stats["BLK"] - min_BLK) / (max_BLK - min_BLK)*0.125)
     normalized_stats.append((stats["A/T"] - min_AT) / (max_AT - min_AT)*0.125)
     normalized_stats.append(( max_PF - stats["PF"]) / (max_PF - min_PF)*0.125)
     #we hardcap everything at 1
     for i in range(len(normalized_stats)):
-        normalized_stats[i] = min(normalized_stats[i],1.0)
+        normalized_stats[i] = min(normalized_stats[i],99999)
     return normalized_stats
 
 def matchUp(opponent, squad):
 
-    other_team = optimize(extractPlayers(opponent))
+    other_team = (extractPlayers(opponent))
     enemy_avgs = averages((other_team))
+    print("Succesfully got enemies")
+
     squad_avgs = averages(squad)
     print(f"Matchup vs {opponent[:-2]}")
     for category in enemy_avgs:
         print(f"{category}: My Squad: {squad_avgs[category]} Enemy Squad: {enemy_avgs[category]}")
     score = evaluateSquad(other_team, squad)
     print(f"is My Squad better? : {score > 6} , Expected Score of: {score}-{12- score}")
-    return score
 
 
 def extractPlayers(filename = "freeagents.csv"):
     players = []
     with open(filename, newline = '') as csvfile:
         csvdata = csv.reader(csvfile, delimiter=',', quotechar='|')
-        
         for player_info in csvdata:
             
             #format is:Player info,FGM,FGA,FG%,FTM,3PTM,3PTA,3PT%,PTS,REB,AST,STL,BLK,A/T,PF
@@ -198,36 +287,51 @@ def extractPlayers(filename = "freeagents.csv"):
             temp_player["TO"] = temp_player["AST"]/temp_player["A/T"]
             temp_player["PF"] = float(player_info[14 + size_delta])
             players.append(temp_player)
+
     return players
 
-def freeAgents(cur_squad):
+def greedyAlgo(players, team_size = 15):
+    result = []
+    for player in players:
+        result.append((player,sum(normalizedScore([player]))))
+    result = sorted(result, key = lambda info: info[1], reverse= True)
+    for i in range(team_size):
+        result[i] = result[i][0]
+    return result[:team_size]
+
+def freeAgents():
 
     freeAgents = extractPlayers()
-    #add our players to the free agent pool
-    for player in cur_squad:
-        freeAgents.append(dict(player))
-    hill_squad = hillClimb(freeAgents)
-    cur_avgs = averages((cur_squad))
-    optimized_averages = averages(hill_squad)
-    for category in cur_avgs:
-        print(f"{category}: Current Roster: {cur_avgs[category]} Optimized: {optimized_averages[category]}")
-    print(f"Here is our original Squad")
-    for player in cur_squad:
-        print(f"{player["Name"]}")
+    hill_squad = geneticOptimization(freeAgents)
+    print("---------------------------------------------------------------------")
     print(f"Here is our Optimized Squad")
     for player in hill_squad:
-        print(f"{player["Name"]}")
-
+        print(f"{player['Name']}")
+    print("---------------------------------------------------------------------")
     return hill_squad
+
 def main():
     
-    players = extractPlayers("currentroster.csv")
-    roster = freeAgents(players)
-    #roster = optimize(players)
+    roster = extractPlayers("weeklyroster.csv")
+    roster = bruteForce(roster)
+    gene_squad = freeAgents()
+    hill_squad = hillClimb(extractPlayers("weeklyroster.csv"))
+    #roster = bruteForce(players)
     print("-----------------------------------------------------------------------------------------------")
     print(f"Now let's do some theoretical matchups:")
-    for opp in ["Slim reaper.csv", "Jimmy's Buckets.csv", "Dunk Daddies.csv"]:
+    for opp in ["Slim reaper.csv", "Jimmy's Buckets.csv", "Dunk Daddies.csv", "Free throw merchants.csv"]:
 
-        matchUp(opp, roster)
+        matchUp(opp, hill_squad)
+    print("---------------------------------------------------------------------------------------------------")
+    print("Now let's see if we've found the global maximum in this small squad")
+    hill_avg = averages(hill_squad)
+    roster_avg = averages(roster)
+    for category in hill_avg:
+        print(f"Category: {category } hill Squad: {hill_avg[category]} Brute force: {roster_avg[category]}")
+    print(f"Hill squad Score{sum(normalizedScore(hill_squad))} Brute force Score: {sum(normalizedScore(roster))}")
+    print(f"Heres the gene squad:")
+    for player in gene_squad:
+        print(f"{player['Name']}")
+    matchUp("Free throw merchants.csv", gene_squad)
 if __name__ == '__main__':
     main()
