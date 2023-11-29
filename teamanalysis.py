2151e78242321ec998e3c649dfbfbeea04b876d2
SamuelHabtu <SGmagicsam@gmail.com>
2023-11-29 02:30:40 -0700

    O_O we might be at a dead end

--- teamanalysis.py
+++ teamanalysis.py
@@ -259,51 +259,63 @@ def normalizedScore(squad, min_max = False):
     min_fgm = 5030.3 
     max_fgm = 6307.0
     min_3ptm = 1316.9999999999998
-    max_3ptm = 1854.4
+    max_3ptm = 1894.0
     min_ftm =  2302.4999999999995
     maX_ftm = 3240.9
 
     #the handsome non dump stats below:
     min_FG_percent =0.47018040852840315
-    max_FG_percent =  0.51
+    max_FG_percent =  0.722995976477871
     min_ThreePt_percent =  0.35447058190235237
-    max_ThreePt_percent =  0.38145948350296977
+    max_ThreePt_percent =  0.4#0.9361702127659577
     min_REB =  4258.9 
-    max_REB = 6108.0# Assuming this is the upper limit for rebounds 
+    max_REB = 5803.0# Assuming this is the upper limit for rebounds 
     min_AST = 3450.1
     max_AST = 4120.7
     min_STL = 829.9000000000001
-    max_STL = 927.9
+    max_STL = 1066.6000000000001
     min_BLK = 642.6
-    max_BLK = 724.0999999999999
+    max_BLK = 789.0000000000002 # 889.9000000000002(if I get turner from Dunk :flushed:)
     min_AT =  2.037886716281323
-    max_AT = 2.096469212226012
+    max_AT = 2.388038247085092
     min_PF =  -2158.2000000000003
-    max_PF =  -1897.2
+    max_PF =  -1600.900000000000001
     # Normalize each statistic, each stat is also weighted by 1/Number of categories
     normalized_stats = []
     n_categories = 12
-    category_cap = 3.01
+    category_cap = 1.00
     if min_max:
-        n_categories = 9
-
+        n_categories = 6
+    #CATEGORIES i LITEARLLY CANNOT REACH THE MAX ON(RAN THE ALGORITHM only considering these categories and still couldnt beat everyone at it)
     normalized_stats.append((stats["PTS"] - min_Pts)/(max_Pts - min_Pts)*(1/n_categories))
     normalized_stats.append((stats["FGM"] - min_fgm)/(max_fgm - min_fgm)*(1/n_categories))
-    normalized_stats.append((stats["FTM"] - min_ftm)/(maX_ftm - min_ftm)*(1/n_categories))
-
     normalized_stats.append((stats["AST"] - min_AST) / (max_AST - min_AST)*(1/n_categories))
-    normalized_stats.append((stats["3PTM"] - min_3ptm)/(max_3ptm - min_3ptm)*(1/n_categories))
+    normalized_stats.append((stats["FTM"] - min_ftm)/(maX_ftm - min_ftm)*(1/n_categories))
+    #---------------------------------------------------------------------------------------------------------------------------------------------
     normalized_stats.append((stats["BLK"] - min_BLK) / (max_BLK - min_BLK)*(1/n_categories))
-
-
-
-    
-    normalized_stats.append((stats["REB"] - min_REB) / (max_REB - min_REB)*(1/n_categories))
-    normalized_stats.append((stats["3PT%"] - min_ThreePt_percent) / (max_ThreePt_percent - min_ThreePt_percent)*(1/n_categories))
     normalized_stats.append((stats["FG%"] - min_FG_percent) / (max_FG_percent - min_FG_percent)*(1/n_categories))
+
+
+    #categories where I am CLOSE to reaching Max or barely beat/lose to the best opponent at this CAT
+
+    normalized_stats.append((stats["REB"] - min_REB) / (max_REB - min_REB)*(1/n_categories))
+
+    #---------------------------------------------------------------------------------------------------------------------------------------------
+
+    #categories where I AM a chad
+
+
+    normalized_stats.append(( stats["PF"]- min_PF) / (max_PF - min_PF)*(1/n_categories))
     normalized_stats.append((stats["A/T"] - min_AT) / (max_AT - min_AT)*(1/n_categories))
     normalized_stats.append((stats["STL"] - min_STL) / (max_STL - min_STL)*(1/n_categories)) 
-    normalized_stats.append(( stats["PF"]- min_PF) / (max_PF - min_PF)*(1/n_categories))
+
+    
+    normalized_stats.append((stats["3PTM"] - min_3ptm)/(max_3ptm - min_3ptm)*(1/n_categories))
+    normalized_stats.append((stats["3PT%"] - min_ThreePt_percent) / (max_ThreePt_percent - min_ThreePt_percent)*(1/n_categories))
+
+    #---------------------------------------------------------------------------------------------------------------------------------------------
+
+
 
     best_stats = []
     #weight everything AND then we also cap the values at 15% higher because realistically 10% should be a good enough margin
@@ -312,8 +324,8 @@ def normalizedScore(squad, min_max = False):
     if min_max:
 
         for i in range(12 - n_categories):
-            normalized_stats[i] = 0
-        #normalized_stats[12 - n_categories - 1] = normalized_stats[12 - n_categories - 1]/2
+            normalized_stats[i] = normalized_stats[i]/30
+        #normalized_stats[12 - n_categories] = normalized_stats[12 - n_categories]/10
     for i in range(len(normalized_stats)):
         normalized_stats[i] = min(normalized_stats[i],category_cap *(1/n_categories))
 
