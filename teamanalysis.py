da3cc166067505259858884266fc82a0d92158d8
SamuelHabtu <SGmagicsam@gmail.com>
2023-12-02 02:25:53 -0700

    Cursed hours commit

--- teamanalysis.py
+++ teamanalysis.py
@@ -297,23 +297,27 @@ def normalizedScore(squad, min_max = False):
     category_cap = 1
     if min_max:
         n_categories = 6
-
+#cant max out even if solo category
     normalized_stats.append((stats["PTS"] - min_Pts)/(max_Pts - min_Pts)*(1/n_categories))
     normalized_stats.append((stats["FTM"] - min_ftm)/(maX_ftm - min_ftm)*(1/n_categories))
     normalized_stats.append((stats["FGM"] - min_fgm)/(max_fgm - min_fgm)*(1/n_categories))
+    normalized_stats.append((stats["AST"] - min_AST) / (max_AST - min_AST)*(1/n_categories))
+
+#can actually max out
+
     normalized_stats.append((stats["3PTM"] - min_3ptm)/(max_3ptm - min_3ptm)*(1/n_categories))
     normalized_stats.append((stats["REB"] - min_REB) / (max_REB - min_REB)*(1/n_categories))
 
+
+    #category used in calculation
     normalized_stats.append((stats["STL"] - min_STL) / (max_STL - min_STL)*(1/n_categories)) 
-    normalized_stats.append((stats["AST"] - min_AST) / (max_AST - min_AST)*(1/n_categories))
-
-
-    
     normalized_stats.append((stats["PF"]- min_PF) / (max_PF - min_PF)*(1/n_categories))
     normalized_stats.append((stats["A/T"] - min_AT) / (max_AT - min_AT)*(1/n_categories))
     normalized_stats.append((stats["FG%"] - min_FG_percent) / (max_FG_percent - min_FG_percent)*(1/n_categories))
     normalized_stats.append((stats["3PT%"] - min_ThreePt_percent) / (max_ThreePt_percent - min_ThreePt_percent)*(1/n_categories))
     normalized_stats.append((stats['BLK'] - min_BLK)/ (max_BLK - min_BLK))
+
+
 
 
 
@@ -331,7 +335,8 @@ def normalizedScore(squad, min_max = False):
 
         for i in range(12 - n_categories):
             normalized_stats[i] = normalized_stats[i]*0
-        #normalized_stats[12 - n_categories] = normalized_stats[12 - n_categories]/2
+        #normalized_stats[12 - n_categories] = normalized_stats[12 - n_categories]
+
     #let's enforce our cap >:^)
     for i in range(len(normalized_stats)):
         normalized_stats[i] = min(normalized_stats[i],category_cap *(1/n_categories))
